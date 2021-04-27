2dc4fe75d8ded210ad8dd90f35c719ac8de10234
unknown <esrrarizk@gmail.com>
2021-04-28 00:46:33 +0200

    my first time

---
+++ task3 bootstrab/task2bootsrab.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    $('.cards-box').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                dots: true,
+                arrows:false,
+                responsive: [
+    {
+      breakpoint: 1330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+                
+
+            });
+})   
+  
+
+
+
+
+
+
+
