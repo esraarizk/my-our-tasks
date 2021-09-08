9a860b85326fb5dce6ea081a259594d7a9596254
eng esraa rizk <esrrarizk@gmail.com>
2021-09-08 21:48:45 +0200

    my first time

---
+++ login page edit/resturant.js
@@ -0,0 +1,16 @@
+ /*function myFunction() {
+  // Get the value of the input field with id="numb"
+  let x = document.getElementById("password").value;
+  let y = document.getElementById("name").value;
+  // If x is Not a Number or less than one or greater than 10
+  let text;
+  if (isNaN(x) || x < 4 || x > 10) {
+    text = "Input not valid";
+    document.getElementById("valid").innerHTML = text;
+ 
+  } else {
+   // text = "Input OK";
+  }
+ 
+}*/
+ 
